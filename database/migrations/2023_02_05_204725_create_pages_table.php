1fdc8cdd4fdd5e3dcd377adcf59589dc7dcc36db
Klim <subbotin.klim@gmail.com>
2023-02-05 21:50:32 +0100

    add page migration

---
+++ database/migrations/2023_02_05_204725_create_pages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->string('type')->default('default');
+            $table->string('description');
+            $table->string('preview')->nullable();
+            $table->text('body')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
