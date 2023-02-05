53b0862048494a767709f37a771e12a24fe19ac5
Klim <subbotin.klim@gmail.com>
2023-02-05 21:49:20 +0100

    add article model

---
+++ app/Models/Article.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Article extends Model
+{
+    use HasFactory, SoftDeletes, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'body',
+        'preview',
+        'seo_title',
+        'seo_description',
+        'seo_keywords',
+        'state',
+    ];
+}
