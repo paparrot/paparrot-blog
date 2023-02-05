b92ef86eb16441618606c4bc1cbf137abbe01a7a
Klim <subbotin.klim@gmail.com>
2023-02-05 21:50:20 +0100

    add tags factory

---
+++ database/factories/TagFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Tag;
+
+class TagFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Tag::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(4),
+            'slug' => $this->faker->slug,
+        ];
+    }
+}
